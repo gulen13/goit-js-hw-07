c92072d606697aca0fd695658e9ec1b48602ec2c
gulen13 <vitaliy.gulen@gmail.com>
2023-03-04 10:12:04 +0200

    1 task is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function createGallery(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  }).join('');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return
+  }
+  const largeUrl = event.target.dataset.source;
+
+  const modal = basicLightbox.create(`<img src="${largeUrl}">`, {
+    onShow: () => {
+      document.addEventListener("keydown", closeOnEscape);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", closeOnEscape);
+    }
+  });
+
+  modal.show()
+
+  function closeOnEscape(event) {
+    if (event.code !== "Escape") {
+      return
+    }
+    modal.close();
+  }
+}
